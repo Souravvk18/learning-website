bc0be8587c2e4a31f7bd557377b142d3b61adad7
Sourav <souravlakshan6@gmail.com>
2022-02-11 09:35:26 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+// ------------------menu icon for mobile------
+
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+// ----------slider section---------------
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+//next function
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+//preview function
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+//autoplay
+setInterval(next, 7000); //slide changes every 7sec
