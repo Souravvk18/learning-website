4b973e780d1939e924cc924c84c18ec312585107
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-04-04 03:11:34 +0530

    Backend Work

--- index.php
+++ index.php
@@ -21,41 +21,38 @@
   $password = $_POST['password'];
   $sql = "INSERT INTO `buie_learning`.`register` ('name`, `gender`, `age`, `mobilenumber`, `email`, `userid`, `password`) VALUES ('$name', '$gender', '$age', '$mobilenumber', '$email', '$userid', '$password');";
 
-  echo $sql;
 
+/*
    if($con->query($sql)==true){
       echo "REGISTER Successfull";
    }else {
-      echo "ERROR: $sql <br> $son->error";
+      echo "ERROR: $sql <br /> $son->error";
    }
 
    $conn->close();
-
-/*
-$name = $_POST['name'];
-$gender = $_POST['gender'];
-$age = $_POST['age'];
-$mobilenumber = $_POST['mobilenumber'];
-$email = $_POST['email'];
-$userid = $_POST['userid'];
-$password = $_POST['password'];
-
-
-// // Database connection_aborted
-
-// // test is the table name
-
-   new mysqli('localhost','root','','test');
-      if($conn->connect_error){
-      die('Connection Failed :' $conn->connect_error);
-    }else{
-     $conn->prepare(("insert into registration(name, gender, age, mobilenumber,email,userid,password)
-     values (?, ?, ?, ?, ?, ?, ?)");
-     $stmt->bind_param("register",$name, $gender, $age, $mobilenumber, $email, $userid, $password);
-     $stmt->execute();
-     echo "Registration Successfull";
-     $stmt->close();
-     $conn->close();
-    }
 */
+
+
+//   $name = $_POST['name'];
+//   $mobilenumber = $_POST['mobilenumber'];
+//   $email = $_POST['email'];
+//   $password = $_POST['password'];
+//
+// // Database connection_aborted
+//
+// // test is the table name
+//
+//   new mysqli('localhost','root','','test');
+//   if($conn->connect_error){
+//     die('Connection Failed :' $conn->connect_error);
+//   }else{
+//     $conn->prepare(("insert into registration(name, mobilenumber, email, password)
+//     values (?, ?, ?, ?)");
+//     $stmt->bind_param("siss",$name, $mobilenumber, $email,$password);
+//     $stmt->execute();
+//     echo "Registration Successfull";
+//     $stmt->close();
+//     $conn->close();
+//   }
+
 ?>
