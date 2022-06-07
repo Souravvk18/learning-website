af0624a9cd42f629b930798689de55f219d8dffa
Kuntal Ojha <kuntalojha2021@gmail.com>
2022-06-07 19:45:48 +0530

    Update Login & Register

--- login.php
+++ login.php
@@ -4,24 +4,32 @@
 
     // Database Connection here
     $con = new mysqli('localhost','root','','buie_learning');
-    if($con->connect_error){
+    if($con->connect_error)
+    {
       die('Connection Failed :'.$con->connect_error);
-    }else {
+    }
+    else
+    {
       $stmt = $con->prepare("select * from register where email = ?");
       $stmt->bind_param("s",$email);
       $stmt->execute();
       $stmt_result = $stmt->get_result();
       if ($stmt_result->num_rows > 0) {
         $data = $stmt_result-> fetch_assoc();
-        if($data['password']===$password){
+        if($data['password']===$password)
+        {
           echo "<script>location.href='home_page.html'</script>";
-        }else {
+        }
+        else 
+        {
           echo "<script>
           alert('Invalid Email or Password')
           </script>";
           echo "<script>location.href='login_page.html'</script>";
         }
-      }else {
+      }
+      else
+      {
         echo "<script>
         alert('Invalid Email or Password')
         </script>";
